1dff2c80be338764dc864bbc59f1971bc3e8c5af
Dylan <dquesada186@gmail.com>
2018-02-26 17:07:00 -0600

    initial commit

---
+++ app/screens/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ScrollView, Text, Linking, View } from "react-native";
+import { Card, Button } from "react-native-elements";
+
+export default () => (
+  <View style={{ flex: 1 }}>
+
+  </View>
+);
